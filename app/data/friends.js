76a38b51c3660a45027f04a76050c14cc1da6093
Joshua Cassell <Joshua Cassell>
2016-07-31 23:54:19 -0400

    framework

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+//building an array of objects and placing
+//it in an array called newMemebers
+var newMembers = [
+{
+  name:'Joshua Cassell',
+  photo: 
+  scores:[
+      '5',
+      '2',
+      '5',
+      '3',
+      '1',
+      '4',
+      '5',
+      '4',
+      '2',
+      '1'
+  ]
+
+},
+
+{
+  name: 'Michael Brown',
+  photo:
+  scores:[]
+},
+
+{
+  name: 'Julio Warren'
+  photo: 
+  scores:[]
+},
+{
+  name:'Ahmad Singh'
+  photo: 
+  scores:[]
+},
+
+{
+  name: 'Nathaniel Barnes'
+  photo: 
+  scores:[]
+}
+  
+]
